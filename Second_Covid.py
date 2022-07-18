ad4f16d43dfa5d21fb1837797c9b36be0459fdb9
HurricaneJar <106694576+HurricaneJar@users.noreply.github.com>
2022-07-18 12:28:14 -0700

    Add files via upload
    Thank you so much for checking my work out!
    This is my final draft for the project.

--- Second_Covid.py
+++ Second_Covid.py
@@ -6,9 +6,11 @@ from csv import reader
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as plotme
 
 covid =pd.read_csv("/Users/zachpolitz/Desktop/Conditions_Contributing_to_COVID-19_Deaths__by_State_and_Age__Provisional_2020-2021.csv")
 headers = covid.columns
+
 
 #       #
 # Clean #
@@ -20,23 +22,23 @@ for row in headers:
 null_counts = covid[covid['COVID-19 Deaths']== '']
  
 data_cropped = covid[['Group','Year','State','Condition','Age Group','COVID-19 Deaths']]
-data_2021_by_state = data_cropped.loc[(data_cropped['Group'] == 'By Year' ) &
-                                      (data_cropped['Year'] == 2021 )]
+data_2020_by_state = data_cropped.loc[(data_cropped['Group'] == 'By Year' ) &
+                                      (data_cropped['Year'] == 2020 )]
 
 #null values are represented as '', so I replace any instances of '' with 0.
-fix_me = data_2021_by_state['COVID-19 Deaths'] 
+fix_me = data_2020_by_state['COVID-19 Deaths'] 
 for f in range(0, len(fix_me)):
     if fix_me.iloc[f] == '':
         fix_me.iloc[f] = 0
     elif pd.isna(fix_me.iloc[f]) == True:
         fix_me.iloc[f] = 0
-data_2021_by_state['COVID-19 Deaths'] = fix_me
+data_2020_by_state['COVID-19 Deaths'] = fix_me
 
 #        #
 # Rename #
 #        #
 working_columns = ['group','year','state','condition','age','deaths']
-data_2021_by_state.columns = working_columns
+data_2020_by_state.columns = working_columns
 
 condition_dict = {'Influenza and pneumonia': 'I&P',
                   'Chronic lower respiratory diseases': 'CLRD',
@@ -67,12 +69,12 @@ condition_dict = {'Influenza and pneumonia': 'I&P',
 # Restructure Data #
 #                  #
 
-all_ages = data_2021_by_state[data_2021_by_state['age'] == 'All Ages'].copy()
-filtered_all_ages = all_ages[['deaths','state']]
-grouped_states = filtered_all_ages.groupby(['state'])['deaths'].sum().reset_index()
+sum_ages = data_2020_by_state[data_2020_by_state['age'] != 'All Ages'].copy()
+filtered_sum_ages = sum_ages[['deaths','state']]
+grouped_states = filtered_sum_ages.groupby(['state'])['deaths'].sum().reset_index()
 sorted_states_table = grouped_states.sort_values(by = 'deaths', ascending = False)
 sorted_states = sorted_states_table['state']
- 
+
 #      #
 # Plot #
 #      #
@@ -81,13 +83,16 @@ columns = 9
 rows = 6
 
 fig, ax = plt.subplots(nrows = rows, ncols = columns, figsize = (14,8))
+fig.suptitle('Conditions_Contributing_to_COVID-19_Deaths__by_State_and_Age__Provisional_2020-2021.csv (Source: CDC, August 2021)')
+fig.text(x= .2, y= .94, s= 'Plots show total annual deaths by condition and age group(color-coded) for 2020 (states are in order of total deaths)')
+
 
 state_index = 0
 for rs in range (0,rows):
     for cs in range (0,columns):
         print(sorted_states.iloc[state_index])
-        state_data = data_2021_by_state.loc[(data_2021_by_state['state'] == sorted_states.iloc[state_index]) &
-                                            (data_2021_by_state['age'] != 'All Ages')]
+        state_data = data_2020_by_state.loc[(data_2020_by_state['state'] == sorted_states.iloc[state_index]) &
+                                            (data_2020_by_state['age'] != 'All Ages')]
 
         filtered_list_to_sort = {'cond':[],'deaths':[]}
         filtered_list_to_sort = pd.DataFrame(filtered_list_to_sort)	
@@ -125,35 +130,46 @@ for rs in range (0,rows):
 
         ylimits = []
         ylabels = []
-        ax[rs,cs].tick_params(axis = 'x', labelsize = 3, rotation = 90)
+        ax[rs,cs].tick_params(axis = 'x', labelsize = 4, rotation = 90)
         ax[rs,cs].tick_params(axis = 'y', labelsize = 6)
-        ax[rs,cs].set_title(sorted_states.iloc[state_index], fontsize = 6)
+        ax[rs,cs].yaxis.tick_right()
+        ax[rs,cs].set_title(sorted_states.iloc[state_index], y=.6, fontsize = 7)
+        ax[rs,cs].spines['right'].set_visible(False)
+        ax[rs,cs].spines['left'].set_visible(False)
+        ax[rs,cs].spines['top'].set_visible(False)
         
         if sorted_states.iloc[state_index] == 'United States':
-            ax[rs,cs].set_ylim([0,500000])
-            ax[rs,cs].set_yticks([500000])
-            ax[rs,cs].set_yticklabels(['500000'])
-            ax[rs,cs].legend(['0-24','25-34','35-44','45-54','55-64','65-74','75-84','85+','Unspecified'])
-            ax[rs,cs].set_title('Death Count of Covid-related Conditions, Provincially (CDC)')
+            lim = bar_base.max()
+            lim_string = str(lim)
+            ax[rs,cs].set_ylim([0,lim])
+            ax[rs,cs].set_yticks([lim])
+            ax[rs,cs].set_yticklabels([lim_string[0:-2]])
 
-        elif sorted_states.iloc[state_index] == 'California':    # Standardized Ylimits
+
+        elif sorted_states.iloc[state_index] == 'California':     # Standardized Ylimits
+            lim = bar_base.max()
+            lim_string = str(lim)
             ax[rs,cs].set_ylim([0,40000])
-            ax[rs,cs].set_yticks([40000])
-            ax[rs,cs].set_yticklabels(['40k'])       
+            ax[rs,cs].set_yticks([lim])
+            ax[rs,cs].set_yticklabels([lim_string[0:-2]])       
 
         else:
-#            lim = bar_base.max()                                 # Custom Ylimits
+#            lim = bar_base.max()                                 # Custom Ylimits 
 #            ylabel_long = str(lim)
 #            ylabel_short = ylabel_long[0:-2]
 #            ax[rs,cs].set_ylim([0,(lim+(.1*lim))])
 #            ax[rs,cs].set_yticks([lim])
 #            ax[rs,cs].set_yticklabels([ylabel_short])
 
-            ax[rs,cs].set_ylim([0,40000])                      # Standardized Ylimits
-            ax[rs,cs].set_yticks([])
-            ax[rs,cs].set_yticklabels([])       
-     
+            lim = bar_base.max()                                  # Standardized Ylimits
+            lim_string = str(lim)
+            ax[rs,cs].set_ylim([0,40000])                     
+            ax[rs,cs].set_yticks([lim])
+            ax[rs,cs].set_yticklabels([lim_string[0:-2]])  
+            
+
         state_index += 1 
+
 
 plt.tight_layout()
 plt.show()
